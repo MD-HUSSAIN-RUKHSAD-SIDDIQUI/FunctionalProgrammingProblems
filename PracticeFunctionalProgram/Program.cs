98c9bf98afbe1f7cdd69d9b9f996228920b866ff
MD HUSSAIN <mdhussainrukhsadsiddiqui@gmail.com>
2023-05-04 23:12:36 +0530

    Welcome to Functional Programming Practice

---
+++ PracticeFunctionalProgram/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeFunctionalProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Functional Programming Practice");
+            Console.ReadKey();
+        }
+    }
+}
